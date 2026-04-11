80205bb9f7cab18d15d4e60ccb9f6d72ea0b2308
Mark Ferrell <major@homeonderanged.org>
2026-04-10 17:21:39 -0700

    feat: add Keyring interface and SystemKeyring implementation

---
+++ internal/keyring.go
@@ -0,0 +1,30 @@
+package internal
+
+import (
+	"github.com/zalando/go-keyring"
+)
+
+// Keyring abstracts system keyring operations for testing.
+type Keyring interface {
+	Get(service, account string) (string, error)
+	Set(service, account, password string) error
+	Delete(service, account string) error
+}
+
+// SystemKeyring delegates to go-keyring's package-level functions.
+type SystemKeyring struct{}
+
+// Get retrieves a secret from the system keyring.
+func (SystemKeyring) Get(service, account string) (string, error) {
+	return keyring.Get(service, account)
+}
+
+// Set stores a secret in the system keyring.
+func (SystemKeyring) Set(service, account, password string) error {
+	return keyring.Set(service, account, password)
+}
+
+// Delete removes a secret from the system keyring.
+func (SystemKeyring) Delete(service, account string) error {
+	return keyring.Delete(service, account)
+}
